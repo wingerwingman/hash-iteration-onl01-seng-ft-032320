be7d5f6467d7e14767a1c251f6b1f5be429f3ba8
Justin Winger <justinwinger4@gmail.com>
2020-03-26 16:23:31 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,7 +8,7 @@ suite_e: "Crumpet the Elf"
 
 def select_winner(passengers)
   winner = ""
-  passenger.each do |suite, name|
+  passengers.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
     winner = name
   end
