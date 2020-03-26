3ddf12161faa3a57553491bc72bd9ecde16a118f
Justin Winger <justinwinger4@gmail.com>
2020-03-26 16:27:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@ birthday_kids = {
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} Years old!"
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
 
