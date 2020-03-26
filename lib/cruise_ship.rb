5249c90116d770fb4647ca937d21f48a59a8651f
Justin Winger <justinwinger4@gmail.com>
2020-03-26 16:21:17 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -5,6 +5,8 @@ suite_c: "Alfred Tennyson",
 suite_d: "Charlie Chaplin", 
 suite_e: "Crumpet the Elf"
 }
+
+def select_winner(passengers)
   winner = ""
   passenger.each do |suite, name|
     if suite == :suite_a && name.start_with?("A")
@@ -15,6 +17,6 @@ end
 winner
 puts winner
 
-def select_winner(passengers)
-
 end
+
+puts select_winner(passangers)
