82a6e7aecf923ddb839803a5a9577566cb956805
Justin Winger <justinwinger4@gmail.com>
2020-03-26 16:30:00 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,9 @@ birthday_kids = {
 
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
+    if age <= 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
   end
 end
 
